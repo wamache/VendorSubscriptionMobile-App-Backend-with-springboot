22254adc5823a6cd1fa5fb3644c869084604befa
wamache <79418155+wamache@users.noreply.github.com>
2024-12-09 09:43:32 +0300

    Update MpesaService.java

--- Backend/src/main/java/com/MobileApp/Backend/service/MpesaService.java
+++ Backend/src/main/java/com/MobileApp/Backend/service/MpesaService.java
@@ -1,71 +1,4 @@
 package com.MobileApp.Backend.service;
-//
-//
-//import com.MobileApp.Backend.config.MpesaConfig;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.reactive.function.client.WebClient;
-//
-//import java.util.Base64;
-//
-//@Service
-//public class MpesaService {
-//
-//    @Autowired
-//    private MpesaConfig mpesaConfig;
-//
-//    @Autowired
-//    private WebClient.Builder webClientBuilder;
-//
-//    // Get Mpesa OAuth token
-//    public String getAccessToken() {
-//        String credentials = mpesaConfig.getConsumerKey() + ":" + mpesaConfig.getConsumerSecret();
-//        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
-//
-//        return webClientBuilder.build()
-//                .post()
-//                .uri(mpesaConfig.getBaseUrl() + "/oauth/v1/generate?grant_type=client_credentials")
-//                .header("Authorization", "Basic " + encodedCredentials)
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .map(response -> {
-//                    // Extract token from the response
-//                    return response.split("\"access_token\":\"")[1].split("\"")[0];
-//                })
-//                .block();
-//    }
-//
-//    // Initiate STK Push
-//    public String initiateSTKPush(String phoneNumber, double amount, String accountReference, String transactionDesc) {
-//        String token = getAccessToken();
-//        String timestamp = String.valueOf(System.currentTimeMillis());
-//        String password = Base64.getEncoder().encodeToString((mpesaConfig.getShortcode() +
-//                mpesaConfig.getPasskey() + timestamp).getBytes());
-//
-//        String stkPushRequest = "{"
-//                + "\"BusinessShortCode\":\"" + mpesaConfig.getShortcode() + "\","
-//                + "\"Password\":\"" + password + "\","
-//                + "\"Timestamp\":\"" + timestamp + "\","
-//                + "\"TransactionType\":\"CustomerPayBillOnline\","
-//                + "\"Amount\":\"" + amount + "\","
-//                + "\"PartyA\":\"" + phoneNumber + "\","
-//                + "\"PartyB\":\"" + mpesaConfig.getShortcode() + "\","
-//                + "\"PhoneNumber\":\"" + phoneNumber + "\","
-//                + "\"CallBackURL\":\"" + mpesaConfig.getCallbackUrl() + "\","
-//                + "\"AccountReference\":\"" + accountReference + "\","
-//                + "\"TransactionDesc\":\"" + transactionDesc + "\""
-//                + "}";
-//
-//        return webClientBuilder.build()
-//                .post()
-//                .uri(mpesaConfig.getBaseUrl() + "/mpesa/stkpush/v1/processrequest")
-//                .header("Authorization", "Bearer " + token)
-//                .bodyValue(stkPushRequest)
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
-//    }
-//}
 import com.MobileApp.Backend.DTO.MpesaStkRequest;
 import com.MobileApp.Backend.DTO.MpesaTokenResponse;
 import org.springframework.http.*;
@@ -79,9 +12,9 @@ import java.time.format.DateTimeFormatter;
 @Service
 public class MpesaService {
 
-    private final String consumerKey = "W0pmY712MVuTuxTcu6JqHJ4fEJwWCZwk";
-    private final String consumerSecret = "mcLoGXtoNGVtSNuC";
-    private final String shortCode = "0704971637";
+    private final String consumerKey = "W0pmY712MVuTuxTcu6JqHJ4fEJwWCZwkb";
+    private final String consumerSecret = "mcLoGXtoNGVtSNuCr";
+    private final String shortCode = "07049";
     private final String passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
     private final String stkPushUrl = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
     private final String tokenUrl = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
